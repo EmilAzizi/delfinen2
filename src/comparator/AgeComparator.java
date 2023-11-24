0e3a1390f4abc94e3997c887ddab8ee871a287b5
mark4802 <maje0011@stud.kea.dk>
2023-11-24 13:37:14 +0100

    ERROR member

--- src/comparator/AgeComparator.java
+++ src/comparator/AgeComparator.java
@@ -1,11 +1,12 @@
 package comparator;
 
+import .Member;
 import java.util.Comparator;
 
-public class AgeComparator implements Comparator<Member> {
+public class AgeComparator implements Comparator<> {
 
     @Override
     public int compare(Member m1, Member m2){
-        return m1.getSwimmingTime().compareTo(m2.getSwimmingTime);
+        return m1.get().compareTo(m2.getSwimmingTime);
     }
 }
